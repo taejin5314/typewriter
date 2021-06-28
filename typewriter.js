3277f82f0c2f42d86cdb3bb4584cadf2bcb6bdd7
Taejin Jung <taejin5314@gmail.com>
2021-06-28 22:00:52 +0000

    Character animation

--- typewriter.js
+++ typewriter.js
@@ -1,5 +1,13 @@
 const sentence = "hello there from lighthouse labs";
+let index = 0;
 
 for (const char of sentence) {
-  process.stdout.write(char);
+  setTimeout(() => {
+    // print the char here
+    process.stdout.write(char);
+  }, 50 * index++); // <= 1s delay to make it noticeable. Can dial it down later.
 }
+
+setTimeout(() => {
+  console.log();
+}, 50 * index);
